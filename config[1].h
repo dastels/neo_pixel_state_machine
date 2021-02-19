002a67d7a5b465faffc954ed6fc7c087aac45b15
Dhardrian <Dhardrian@users.noreply.github.com>
2021-02-19 13:26:52 -0800

    Add files via upload

---
+++ config[1].h
@@ -0,0 +1,17 @@
+// -*- mode: c++ -*-
+// Any configuration values
+//
+// Copyright (c) 2021 Dave Astels
+
+#ifndef __CONFIG__
+#define __CONFIG__
+
+//******************************************************************************
+// MAKE SURE THESE TWO AGREE
+//******************************************************************************
+
+#define RGBW (0)                // 0 = RGB pixels, 1 = RGBW pixels
+#define PIXEL_TYPE (NEO_GRBW)
+
+
+#endif
